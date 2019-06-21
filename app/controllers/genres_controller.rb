094f6dc488a21155a37281b323aa2a09a2ffd53c
Nastassja <nastassja@platform45.com>
2019-06-21 10:26:06 +0200

    added genre index, create genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,20 @@
 class GenresController < ApplicationController
+def index
+@genres=Genre.all
+end
+
+def new
+@genre=Genre.new
+end
+
+def create
+  @genre=Genre.new(genre_params)
+  @genre.save
+  redirect_to genres_url
+end
+
+private
+def genre_params
+params.require(:genre).permit(:name)
+end
 end
