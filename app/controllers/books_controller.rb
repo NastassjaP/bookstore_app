8cd99841b9c608dfdc5ae7ffb741ec88994b4685
Nastassja <nastassja@platform45.com>
2019-06-24 12:20:17 +0200

    changed root to view books, can click on book to view and update it

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,8 +20,27 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    @user=User.find(current_user.id)
+    @book=@user.books.find(params[:id])
+  end
+
+  def edit
+    @user=User.find(current_user.id)
+    @book=@user.books.find(params[:id])
+  end
+
+  def update
+    @user=User.find(current_user.id)
+    @book=@user.books.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
   private
+
   def book_params
   params.require(:book).permit(:name,:description,:date_published,:genre_id,:user_id)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
- devise_for :users
- resources :books
- resources :genres
-
- devise_scope :user do
-   authenticated :user do
-    root 'pages#home', as: :authenticated_root
-   end
-
-   unauthenticated do
-     root 'devise/sessions#new', as: :unauthenticated_root
-   end
-  end
-
-mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  devise_for :users
+  resources :books
+  resources :genres
+  root 'books#index'
+  resources:pages
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
